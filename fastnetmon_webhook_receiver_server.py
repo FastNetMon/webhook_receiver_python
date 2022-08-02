4cdbc3070a7ea299e36c30ca9bb3b5ae5af6b2fb
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-08-02 21:28:53 +0100

    Added example script

---
+++ fastnetmon_webhook_receiver_server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+import SocketServer
+import random
+import socket
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        
+    def do_GET(self):
+        self._set_headers()
+        f = open("index.html", "r")
+        self.wfile.write(f.read())
+        
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        self._set_headers()
+        print "in post method"
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+        self.send_response(200)
+        self.end_headers()
+        print "Got POST data: " + self.data_string
+        #data = simplejson.loads(self.data_string)
+        #with open("test123456.json", "w") as outfile:
+        #    simplejson.dump(data, outfile)
+        #print "{}".format(data)
+        #f = open("for_presen.py")
+        #self.wfile.write(f.read())
+        return
+        
+class HTTPServerV6(HTTPServer):
+  address_family = socket.AF_INET6
+  
+# To enable IPv6 mode replace HTTPServer by HTTPServerV6 and replace '' by '::'
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+    
+if __name__ == "__main__":
+    from sys import argv
+if len(argv) == 2:
+    run(port=int(argv[1]))
+else:
+    run()
